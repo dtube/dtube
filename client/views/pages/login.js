e368d56260a910ca396e8e6527ac322d4033cb9c
Adrien M <hadolf@gmail.com>
2017-08-06 19:05:01 +0200

    improve login with alerts and shit

--- client/views/pages/login.js
+++ client/views/pages/login.js
@@ -7,19 +7,50 @@ Template.login.helpers({
 Template.login.events({
   'submit .form': function(event) {
     event.preventDefault()
+    var username = event.target.username.value;
     var privatekey = event.target.privatekey.value;
-    var publickey = steem.auth.wifToPublic(privatekey)
-    var user = {
-      privatekey: event.target.privatekey.value
+    if (!username || !privatekey) {
+      toastr.error('Please fill both username and private posting key to login', 'Error')
+      return
     }
-    user.publickey = steem.auth.wifToPublic(user.privatekey)
-    steem.api.getKeyReferences([publickey], function(err, result) {
-      user.username = result[0][0]
-      Waka.db.Users.upsert(user, function() {
-        Users.refreshUsers()
-        Session.set('activeUsername', user.username)
-        FlowRouter.go('/')
-      })
+    steem.api.getAccounts([username], function(err, result) {
+      if (!result || result.length < 1) {
+        toastr.error('Username does not exist', 'Error')
+        return
+      }
+      var chainuser = result[0]
+      var user = {
+        privatekey: event.target.privatekey.value
+      }
+      try {
+        user.publickey = steem.auth.wifToPublic(user.privatekey)
+      } catch (e) {
+        toastr.error('Private Posting Key is wrong', 'Error')
+        return
+      }
+
+      if (chainuser.posting.key_auths[0][0] == user.publickey) {
+        // correct key for the user, loggin in
+        user.username = username
+        Waka.db.Users.upsert(user, function() {
+          Users.refreshUsers()
+          Session.set('activeUsername', user.username)
+          FlowRouter.go('/')
+        })
+      } else {
+        toastr.error('Username and Private Posting Key do not match', 'Error')
+      }
     });
+
+    // getKeyReferences doesn't work for everyone it seems (eg: steeminator3000)
+    // gotta use something else and ask for username unfortunatly
+    // steem.api.getKeyReferences([publickey], function(err, result) {
+    //   user.username = result[0][0]
+    //   Waka.db.Users.upsert(user, function() {
+    //     Users.refreshUsers()
+    //     Session.set('activeUsername', user.username)
+    //     FlowRouter.go('/')
+    //   })
+    // });
   }
 })
