e4f2b8883db2f2a6b37205eb08c252368b170640
koala.jeremy <koala.jeremy@hotmail.fr>
2017-09-04 23:56:03 +0200

    translate with 2 cultures and autodetect language

--- client/translate.js
+++ client/translate.js
@@ -1,18 +1,67 @@
+var jsonTranslate;
+
 function translate(code){
+  //if not loaded yet
+  if(!jsonTranslate){
+    var culture = getCultureAuto();
+    jsonTranslate = loadJsonTranslate(culture);
+  }
+
+  //find traduction
   var value = code;
-
-  //todo get the current culture (en-gb, fr-fr ...) from ?
-  var culture = 'en-gb';
-
-  //todo
-  //try get the file translate-culture.json (mise en cache, refresh ?)
-  //si pas de fichier trouvé, get the file translate-en-gb.json
-  var obj = JSON.parse('{"HOME_TITLE_HOT_VIDEOS":"Hot Videos","HOME_TITLE_TRENDING_VIDEOS":"Trending Videos"}');
-  for(var key in obj)
+  for(var key in jsonTranslate)
     if(key === code)
-      value = obj[key];
+      value = jsonTranslate[key];
 
   return value;
+}
+
+function getCultureAuto(){
+  //default culture
+  var culture = 'en-gb';
+  
+  var listCult;
+  if(navigator.languages){
+    listCult=navigator.languages;
+  }
+  else if(navigator.language){
+    listCult[0] = navigator.language;
+  }
+  else if(navigator.userLanguage){
+    listCult[0] = navigator.userLanguage;
+  }
+  
+  for(var j = 0;j < listCult.length;j++){
+    var cult = listCult[j].toLowerCase();
+    //essaye de trouver du plus spécifique au moins spécifique
+    if(cult === "fr-fr"){
+      culture = "fr-fr";
+      break;
+    }
+    else if(cult.startsWith("fr")){
+      culture = "fr-fr";
+      break;
+    }
+    else if(cult.startsWith("en")){
+      culture = "en-gb";
+      break;
+    }
+    else{
+      culture = "en-gb";
+      break;
+    }
+  }
+  return culture;
+}
+
+function loadJsonTranslate(culture){
+  switch(culture){
+    case "fr-fr":
+      return JSON.parse('{"HOME_TITLE_HOT_VIDEOS":"Videos chaudes","HOME_TITLE_TRENDING_VIDEOS":"Videos tendances"}');
+
+    case "en-gb":
+      return JSON.parse('{"HOME_TITLE_HOT_VIDEOS":"Hot Videos","HOME_TITLE_TRENDING_VIDEOS":"Trending Videos"}');
+  }
 }
 
 //for js files
