d9cade42d94a5b171e8d5ae8422146e9ed12ab23
Adrien M <hadolf@gmail.com>
2017-09-22 22:43:39 +0200

    fix borken links and add 11 new languages

--- client/settings.js
+++ client/settings.js
@@ -29,11 +29,23 @@ Meteor.settings.public = {
   "ipns": "Qmb8hgdQoyotsnUj3JKWvWzcfA9Jx4Ak2ao1XzCVLfDtuB",
   "translations": {
     "en-us": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170905t000249923z'},
+    "es-es": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t201038814z'},
+    "es-mx": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t201104670z'},
+    "es-ve": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t201216550z'},
     "fr-fr": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170905t002032311z'},
     "ko-kr": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170919t133419412z'},
     "pt-pt": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170919t134820783z'},
+    "zh-cn": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t195803253z'},
+    "ar": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t195653064z'},
+    "da": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t195952809z'},
+    "de": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t200309127z'},
+    "et": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t200641990z'},
+    "hr": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t200823074z'},
+    "id": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t200854549z'},
+    "it": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t201636123z'},
     "lt": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170919t135235158z'},
-    "tr": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170919t135734303z'},
-    "pl": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170919t140006611z'}
+    "nl": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170922t200933109z'},
+    "pl": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170919t140006611z'},
+    "tr": {author: 'curator', permlink: 're-curator-dtube-translations-root-20170919t135734303z'}
   }
 }
