eefa77c5a33270e2991f67ca2b919ade5f3bf023
Hightouch <cetinkaya.ozden@gmail.com>
2017-11-20 20:59:11 +0100

    shares

--- client/views/pages/video/video.js
+++ client/views/pages/video/video.js
@@ -229,50 +229,6 @@ Template.video.events({
       $('#sharesegment').removeClass('subcommentsclosed');
     }
     shareIsOpen = !shareIsOpen
-  },
-  'click .ui.button.copy': function () {
-    var text = document.getElementById('permalink').value;
-    console.log(text)
-    if (window.clipboardData && window.clipboardData.setData) {
-      return clipboardData.setData("Text", text);
-    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-      var textarea = document.createElement("textarea");
-      textarea.textContent = text;
-      textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
-      document.body.appendChild(textarea);
-      textarea.select();
-      try {
-        return document.execCommand("copy");  // Security exception may be thrown by some browsers.      
-      } catch (ex) {
-        toastr.error('VIDEO_LINK_ERROR')
-        return false;
-      } finally {
-        toastr.success(translate('VIDEO_PERMA_LINK_COPIED'))                
-        document.body.removeChild(textarea);
-      }
-    }
-  },
-  'click .ui.button.copyembed': function () {
-    var text = document.getElementById('embedlink').value;
-    console.log(text)
-    if (window.clipboardData && window.clipboardData.setData) {
-      return clipboardData.setData("Text", text);
-    } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-      var textarea = document.createElement("textarea");
-      textarea.textContent = text;
-      textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in MS Edge.
-      document.body.appendChild(textarea);
-      textarea.select();
-      try {
-        return document.execCommand("copy");  // Security exception may be thrown by some browsers.
-      } catch (ex) {
-        toastr.error('VIDEO_LINK_ERROR')
-        return false;
-      } finally {
-        toastr.success(translate('VIDEO_EMBED_LINK_COPIED'))                        
-        document.body.removeChild(textarea);
-      }
-    }
   }
 })
 
