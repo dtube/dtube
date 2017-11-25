5f15cc6a809820c125f0ac093447818b16591ac8
Hightouch <cetinkaya.ozden@gmail.com>
2017-11-25 15:20:33 +0100

    css fixes

--- client/views/pages/login/login.js
+++ client/views/pages/login/login.js
@@ -7,58 +7,69 @@ Template.login.helpers({
 Template.login.events({
   'submit .form': function(event) {
     event.preventDefault()
+    //TOCHECK
+    var currentUser = Session.get('activeUsername')
     var username = event.target.username.value.toLowerCase().replace('@','');
-    var privatekey = event.target.privatekey.value;
-    if (!username || !privatekey) {
-      toastr.error(translate('LOGIN_ERROR_EMPTY_USERNAME_POSTING_KEY'), translate('ERROR_TITLE'))
-      return
+    if (currentUser == username)
+    {
+      toastr.error(translate('LOGIN_ERROR_ALREADY_LOGGED'), translate('ERROR_TITLE'))
     }
-    steem.api.getAccounts([username], function(err, result) {
-      if (!result || result.length < 1) {
-        toastr.error(translate('LOGIN_ERROR_UNKNOWN_USERNAME'), translate('ERROR_TITLE'))
+    else
+    {
+      var username = event.target.username.value.toLowerCase().replace('@','');
+      var privatekey = event.target.privatekey.value;
+      if (!username || !privatekey) {
+        toastr.error(translate('LOGIN_ERROR_EMPTY_USERNAME_POSTING_KEY'), translate('ERROR_TITLE'))
         return
       }
-      var chainuser = result[0]
-      var user = {
-        privatekey: event.target.privatekey.value
-      }
-      try {
-        user.publickey = steem.auth.wifToPublic(user.privatekey)
-      } catch (e) {
-        toastr.error(translate('LOGIN_ERROR_WRONG_POSTING_KEY'), translate('ERROR_TITLE'))
-        return
-      }
-
-      if (chainuser.posting.key_auths[0][0] == user.publickey) {
-        // correct key for the user, loggin in
-        user.username = username
-        if (event.target.rememberme.checked) {
-          Waka.db.Users.upsert(user, function() {
-            Users.remove({})
-            Users.refreshLocalUsers()
+      steem.api.getAccounts([username], function(err, result) {
+        if (!result || result.length < 1) {
+          toastr.error(translate('LOGIN_ERROR_UNKNOWN_USERNAME'), translate('ERROR_TITLE'))
+          return
+        }
+        var chainuser = result[0]
+        var user = {
+          privatekey: event.target.privatekey.value
+        }
+        try {
+          user.publickey = steem.auth.wifToPublic(user.privatekey)
+        } catch (e) {
+          toastr.error(translate('LOGIN_ERROR_WRONG_POSTING_KEY'), translate('ERROR_TITLE'))
+          return
+        }
+  
+        if (chainuser.posting.key_auths[0][0] == user.publickey) {
+          // correct key for the user, loggin in
+          user.username = username
+          if (event.target.rememberme.checked) {
+            Waka.db.Users.upsert(user, function() {
+              Users.remove({})
+              Users.refreshLocalUsers()
+              Session.set('activeUsername', user.username)
+              Videos.loadFeed(user.username)
+              FlowRouter.go('#!/')
+            })
+          } else {
+            Users.insert(user)
             Session.set('activeUsername', user.username)
             Videos.loadFeed(user.username)
+            Users.refreshUsers([user.username])
             FlowRouter.go('#!/')
-          })
+            steem.api.getAccounts([user.username], function(e, chainusers) {
+              for (var i = 0; i < chainusers.length; i++) {
+                var user = Users.findOne({username: chainusers[i].name})
+                if (chainusers[i].json_metadata && JSON.parse(chainusers[i].json_metadata))
+                  user.json_metadata = JSON.parse(chainusers[i].json_metadata)
+                Users.update({username: user.username}, user)
+              }
+            })
+          }
         } else {
-          Users.insert(user)
-          Session.set('activeUsername', user.username)
-          Videos.loadFeed(user.username)
-          Users.refreshUsers([user.username])
-          FlowRouter.go('#!/')
-          steem.api.getAccounts([user.username], function(e, chainusers) {
-            for (var i = 0; i < chainusers.length; i++) {
-              var user = Users.findOne({username: chainusers[i].name})
-              if (chainusers[i].json_metadata && JSON.parse(chainusers[i].json_metadata))
-                user.json_metadata = JSON.parse(chainusers[i].json_metadata)
-              Users.update({username: user.username}, user)
-            }
-          })
+          toastr.error(translate('LOGIN_ERROR_AUTHENTIFICATION_FAILED'), translate('ERROR_TITLE'))
         }
-      } else {
-        toastr.error(translate('LOGIN_ERROR_AUTHENTIFICATION_FAILED'), translate('ERROR_TITLE'))
-      }
-    });
+      });
+  
+    }
 
     // getKeyReferences doesn't work for everyone it seems (eg: steeminator3000)
     // gotta use something else and ask for username unfortunatly
