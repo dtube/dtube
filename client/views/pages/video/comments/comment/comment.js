50983e63d7273d5e59009b472b1ccd3153d77be4
Adrien M <adrien@d.tube>
2017-12-16 22:58:19 +0100

    fix steemconnect upvote comment

--- client/views/pages/video/comments/comment/comment.js
+++ client/views/pages/video/comments/comment/comment.js
@@ -5,7 +5,7 @@ Template.comment.events({
     var permlink = $(event.target).data('permlink')
     if (!permlink) permlink = $(event.target).parent().data('permlink')
     var weight = Session.get('voteWeight') * -100
-    broadcast.vote(wif, voter, author, permlink, weight, function (err, result) {
+    broadcast.vote(author, permlink, weight, function (err, result) {
       if (err) toastr.error(err.cause.payload.error.data.stack[0].format, translate('GLOBAL_ERROR_COULD_NOT_VOTE'))
       else toastr.success(translate('GLOBAL_ERROR_DOWNVOTE_FOR', weight / 100 + '%', author + '/' + permlink))
       Template.video.loadState()
@@ -17,7 +17,7 @@ Template.comment.events({
     var permlink = $(event.target).data('permlink')
     if (!permlink) permlink = $(event.target).parent().data('permlink')
     var weight = Session.get('voteWeight') * 100
-    broadcast.vote(wif, voter, author, permlink, weight, function (err, result) {
+    broadcast.vote(author, permlink, weight, function (err, result) {
       if (err) toastr.error(err.cause.payload.error.data.stack[0].format, translate('GLOBAL_ERROR_COULD_NOT_VOTE'))
       else toastr.success(translate('GLOBAL_ERROR_VOTE_FOR', weight / 100 + '%', author + '/' + permlink))
       Template.video.loadState()
