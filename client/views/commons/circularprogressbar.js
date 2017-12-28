938fc9a2ab81403aef3da982aed41ec15e4d9a2d
Hightouch <cetinkaya.ozden@gmail.com>
2017-12-28 18:19:49 +0100

    sidebar and scrollbars

--- client/views/commons/circularprogressbar.js
+++
@@ -1,7 +0,0 @@
-Template.circularprogressbar.rendered = function () {
-  $('head style[type="text/css"]').attr('type', 'text/less');
-  less.refreshStyles();
-
-    $('.radial-progress').attr('data-progress', 40);
-  
-}
