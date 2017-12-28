78c0fc210195343a5cb0b3b99e8bbc0827b08c39
Hightouch <cetinkaya.ozden@gmail.com>
2017-12-28 23:31:06 +0100

    lazyload subscribes

--- client/views/sidebar/subscribe/subscriber.js
+++ client/views/sidebar/subscribe/subscriber.js
@@ -2,6 +2,15 @@ Template.subscriber.rendered = function () {
   $('.ui.avatar.image').visibility({
     type       : 'image',
     transition : 'fade in',
-    duration   : 1000
+    duration   : 1000,
+    offset:100,
+    includeMargin:false,
+    initialCheck:false,
+    continuous : true,
+    context: $('#subscriberslist') ,
+    onBottomVisible: function() {
+      // loads a max of 5 times
+      Template.subscriber.rendered()
+    }
   })
 }
