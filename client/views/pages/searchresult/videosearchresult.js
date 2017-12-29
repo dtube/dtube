1c4213f3cec9bae0330bd8054bd2171b06a8dcc3
Hightouch <cetinkaya.ozden@gmail.com>
2017-12-29 01:53:24 +0100

    lazylaod on search

--- client/views/pages/searchresult/videosearchresult.js
+++ client/views/pages/searchresult/videosearchresult.js
@@ -1,3 +1,8 @@
 Template.videosearchresult.rendered = function () {
+  $(this.firstNode.nextSibling).find('img').visibility({
+    type       : 'image',
+    transition : 'fade in',
+    duration   : 1000
+  })
   Template.settingsdropdown.nightMode();
 }
