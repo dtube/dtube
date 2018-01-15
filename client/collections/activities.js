b1eb72a88aeb9171a78b640357e7bc4facc5a3bc
hightouch67 <hightouch@hotmail.fr>
2018-01-15 16:44:58 +0100

    'activity'

--- client/collections/activities.js
+++ client/collections/activities.js
@@ -7,36 +7,36 @@ Activities.getAccountHistory = function (username) {
             console.log(r[i])
             var op = r[i][1].op
             var date = r[i][1].timestamp
-            Activities.filterOperations(op,date)
+            Activities.filterOperations(username,op,date)
         }
     })
 }
 
-Activities.filterOperations = function (op, date) {
+Activities.filterOperations = function (username, op, date) {
     switch (op[0]) {
         case "curation_reward":
-            Activities.insert({ type: 'curation_reward', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'curation_reward', tx: op[1], date : date })
             break;
         case "vote":
-            Activities.insert({ type: 'vote', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'vote', tx: op[1], date : date })
             break;
         case "claim_reward_balance":
-            Activities.insert({ type: 'claim_reward_balance', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'claim_reward_balance', tx: op[1], date : date })
             break;
         case "comment":
-            Activities.insert({ type: 'comment', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'comment', tx: op[1], date : date })
             break;
         case "account_witness_vote":
-            Activities.insert({ type: 'account_witness_vote', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'account_witness_vote', tx: op[1], date : date })
             break;
         case "transfer_to_vesting":
-            Activities.insert({ type: 'transfer_to_vesting', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'transfer_to_vesting', tx: op[1], date : date })
             break;
         case "account_update":
-            Activities.insert({ type: 'account_update', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'account_update', tx: op[1], date : date })
             break;
         case "custom_json":
-            Activities.insert({ type: 'custom_json', tx: op[1], date : date })
+            Activities.insert({username : username, type: 'custom_json', tx: op[1], date : date })
             break;
     }
 }
