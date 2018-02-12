c2c743fd4472939e33a2b8f0defbd818712ce304
Adrien M <adrien@d.tube>
2018-02-12 23:49:01 +0100

    improve errors

--- client/errors.js
+++ client/errors.js
@@ -1,5 +1,11 @@
 Meteor.blockchainError = function(error) {
-  if (error.cause) return error.cause.payload.error.data.stack[0].format
+  if (error.data && error.data.stack)
+    return error.data.stack[0].format
+  if (error.payload && error.payload.error)
+    return error.payload.error.data.stack[0].format
+  if (error.cause && error.cause.payload && error.cause.payload.error)
+    return error.cause.payload.error.data.stack[0].format
+    
   console.log(error)
   return translate('ERROR_UNKNOWN');
 }
--- client/views/pages/upload/uploadform/uploadform.js
+++ client/views/pages/upload/uploadform/uploadform.js
@@ -144,123 +144,119 @@ Template.uploadform.events({
     var tags = Template.uploadform.parseTags(form.tags.value)
     var article = Template.uploadform.generateVideo(form, tags[1])
     $('#step3load').show()
-    Waka.api.Set(article, {}, function (e, r) {
-      Videos.refreshWaka()
-      // publish on blockchain !!
-      var wif = Users.findOne({ username: Session.get('activeUsername') }).privatekey
-      var author = r.article.info.author
-      var permlink = r.article.info.permlink
-      var title = r.article.info.title
-      var body = form.body.value
 
-      if (!body || body.length < 1)
-        body = Template.upload.genBody(author, permlink, title, r.article.info.snaphash, r.article.content.videohash, r.article.content.description)
-      else
-        body = Template.upload.genBody(author, permlink, title, r.article.info.snaphash, r.article.content.videohash, body)
+    // publish on blockchain !!
+    var wif = Users.findOne({ username: Session.get('activeUsername') }).privatekey
+    var author = article.info.author
+    var permlink = article.info.permlink
+    var title = article.info.title
+    var body = form.body.value
+
+    if (!body || body.length < 1)
+      body = Template.upload.genBody(author, permlink, title, article.info.snaphash, article.content.videohash, article.content.description)
+    else
+      body = Template.upload.genBody(author, permlink, title, article.info.snaphash, article.content.videohash, body)
 
 
-      var jsonMetadata = {
-        video: r.article,
-        tags: tags[0],
-        app: Meteor.settings.public.app
-      }
+    var jsonMetadata = {
+      video: article,
+      tags: tags[0],
+      app: Meteor.settings.public.app
+    }
 
-      var percent_steem_dollars = 10000
-      if (form.powerup && form.powerup.checked)
-        percent_steem_dollars = 0
+    var percent_steem_dollars = 10000
+    if (form.powerup && form.powerup.checked)
+      percent_steem_dollars = 0
 
-      var operations = [
-        ['comment',
-          {
-            parent_author: '',
-            parent_permlink: tags[0][0],
-            author: author,
-            permlink: permlink,
-            title: title,
-            body: body,
-            json_metadata: JSON.stringify(jsonMetadata)
-          }
-        ],
-        ['comment_options', {
+    var operations = [
+      ['comment',
+        {
+          parent_author: '',
+          parent_permlink: tags[0][0],
           author: author,
           permlink: permlink,
-          max_accepted_payout: '1000000.000 SBD',
-          percent_steem_dollars: percent_steem_dollars,
-          allow_votes: true,
-          allow_curation_rewards: true,
-          extensions: [
-            [0, {
-              beneficiaries: [{
-                account: Meteor.settings.public.beneficiary,
-                weight: Session.get('remoteSettings').dfees
-              }]
-            }]
-          ]
-        }]
-      ];
-      $('#step3load').show()
-      console.log(operations)
-      broadcast.send(
-        operations,
-        function (e, r) {
-          $('#step3load').hide()
-          if (e) {
-            if (e.payload) toastr.error(e.payload.error.data.stack[0].format, translate('ERROR_TITLE'))
-            else toastr.error(translate('UPLOAD_ERROR_SUBMIT_BLOCKCHAIN'), translate('ERROR_TITLE'))
-          } else {
-            Session.set('uploadedVideo', { author: author, permlink: permlink })
-            FlowRouter.go('/v/' + author + "/" + permlink)
-          }
+          title: title,
+          body: body,
+          json_metadata: JSON.stringify(jsonMetadata)
         }
-      )
-    })
+      ],
+      ['comment_options', {
+        author: author,
+        permlink: permlink,
+        max_accepted_payout: '1000000.000 SBD',
+        percent_steem_dollars: percent_steem_dollars,
+        allow_votes: true,
+        allow_curation_rewards: true,
+        extensions: [
+          [0, {
+            beneficiaries: [{
+              account: Meteor.settings.public.beneficiary,
+              weight: Session.get('remoteSettings').dfees
+            }]
+          }]
+        ]
+      }]
+    ];
+    $('#step3load').show()
+    console.log(operations)
+    broadcast.send(
+      operations,
+      function (e, r) {
+        $('#step3load').hide()
+        if (e) {
+          console.log(e)
+          toastr.error(Meteor.blockchainError(e), translate('ERROR_TITLE'))
+        } else {
+          Session.set('uploadedVideo', { author: author, permlink: permlink })
+          FlowRouter.go('/v/' + author + "/" + permlink)
+        }
+      }
+    )
   },
   'click .editsubmit': function (event) {
     event.preventDefault()
     var form = document.getElementsByClassName('uploadform')[0]
     var tags = Template.uploadform.parseTags(form.tags.value)
     var article = Template.uploadform.generateVideo(form, tags[1])
-    Waka.api.Set(article, {}, function (e, r) {
-      Videos.refreshWaka()
-      var wif = Users.findOne({ username: Session.get('activeUsername') }).privatekey
-      var author = r.article.info.author
-      var permlink = r.article.info.permlink
-      var title = r.article.info.title
-      var body = form.body.value
 
-      var jsonMetadata = {
-        video: r.article,
-        tags: tags[0],
-        app: Meteor.settings.public.app
-      }
+    var wif = Users.findOne({ username: Session.get('activeUsername') }).privatekey
+    var author = Session.get('activeUsername')
+    var permlink = article.info.permlink
+    var title = article.info.title
+    var body = form.body.value
 
-      var operations = [
-        ['comment',
-          {
-            parent_author: '',
-            parent_permlink: tags[0][0],
-            author: author,
-            permlink: permlink,
-            title: title,
-            body: body,
-            json_metadata: JSON.stringify(jsonMetadata)
-          }
-        ]
-      ];
-      console.log(operations)
-      broadcast.send(
-        operations,
-        function (e, r) {
-          if (e) {
-            console.log(e)
-            if (e.payload) toastr.error(e.payload.error.data.stack[0].format, translate('ERROR_TITLE'))
-            else toastr.error(translate('UPLOAD_ERROR_SUBMIT_BLOCKCHAIN'), translate('ERROR_TITLE'))
-          } else {
-            console.log('done')
-          }
+    var jsonMetadata = {
+      video: article,
+      tags: tags[0],
+      app: Meteor.settings.public.app
+    }
+
+    var operations = [
+      ['comment',
+        {
+          parent_author: '',
+          parent_permlink: tags[0][0],
+          author: author,
+          permlink: permlink,
+          title: title,
+          body: body,
+          json_metadata: JSON.stringify(jsonMetadata)
         }
-      )
-    })
+      ]
+    ];
+    console.log(operations)
+    // broadcast.send(
+    //   operations,
+    //   function (e, r) {
+    //     if (e) {
+    //       console.log(e)
+    //       if (e.payload) toastr.error(e.payload.error.data.stack[0].format, translate('ERROR_TITLE'))
+    //       else toastr.error(translate('UPLOAD_ERROR_SUBMIT_BLOCKCHAIN'), translate('ERROR_TITLE'))
+    //     } else {
+    //       console.log('done')
+    //     }
+    //   }
+    // )
   }
 })
 
