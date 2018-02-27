53912fc474e57302c4c66bbde3ebf811d9c2b948
Adrien M <adrien@d.tube>
2018-02-28 00:24:15 +0100

    fix mistake

--- client/views/pages/upload/uploadvideoprogress/uploadvideoprogress.js
+++ client/views/pages/upload/uploadvideoprogress/uploadvideoprogress.js
@@ -41,21 +41,17 @@ Template.uploadvideoprogress.update = function() {
         $('input[name="videohash"]').val(data.ipfsAddSourceVideo.hash)
 
       for (let i = 0; i < data.encodedVideos.length; i++) {
-        if (data.encodedVideos[i].encode) {
-          switch(data.encodedVideos[i].encode.encodeSize || data.encodedVideos[i].ipfsAddEncodeVideo.encodeSize) {
-            case 'F480p':
-              $('input[name="video480hash"]').val(data.encodedVideos[i].ipfsAddEncodeVideo.hash)
-              break;
-            case 'F720p':
-              $('input[name="video720hash"]').val(data.encodedVideos[i].ipfsAddEncodeVideo.hash)
-              break;
-            case 'F1080p':
-              $('input[name="video1080hash"]').val(data.encodedVideos[i].ipfsAddEncodeVideo.hash)
-              break;
-          }
+        switch(data.encodedVideos[i].ipfsAddEncodeVideo.encodeSize || data.encodedVideos[i].encode.encodeSize) {
+          case 'F480p':
+            $('input[name="video480hash"]').val(data.encodedVideos[i].ipfsAddEncodeVideo.hash)
+            break;
+          case 'F720p':
+            $('input[name="video720hash"]').val(data.encodedVideos[i].ipfsAddEncodeVideo.hash)
+            break;
+          case 'F1080p':
+            $('input[name="video1080hash"]').val(data.encodedVideos[i].ipfsAddEncodeVideo.hash)
+            break;
         }
-        
-        
       }
 
       if (data.sprite)
