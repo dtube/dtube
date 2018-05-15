220f860cdfef796b7c8e943d8dedc0978d8e9e96
Adrien M <adrien@d.tube>
2018-05-15 17:34:11 +0200

    remember nightmode setting

--- client/views/topbar/settingsdropdown/settingsdropdown.js
+++ client/views/topbar/settingsdropdown/settingsdropdown.js
@@ -23,12 +23,12 @@ Template.settingsdropdown.rendered = function() {
       } else if (e.hasClass('repogc')) {
         localIpfs.repo.gc()
       } else if (e.hasClass('nightMode')) {
-        if (!Session.get('isInNightMode')) {
+        if (!UserSettings.get('isInNightMode')) {
           Template.settingsdropdown.switchToNightMode();
         } else {
           Template.settingsdropdown.switchToNormalMode();
         }
-        Session.set('isInNightMode', !Session.get('isInNightMode'))
+        UserSettings.set('isInNightMode', !UserSettings.get('isInNightMode'))
       } else {
         //console.log(value,text,e)
       }
@@ -64,7 +64,7 @@ Template.settingsdropdown.helpers({
     return Session.get('localIpfs')
   },
   isInNightMode:function() {
-    return Session.get('isInNightMode')
+    return UserSettings.get('isInNightMode')
   },
   languages: function() {
     return Meteor.settings.public.lang
@@ -74,127 +74,127 @@ Template.settingsdropdown.helpers({
 
 Template.settingsdropdown.nightMode = function()
 {
-  if (Session.get('isInNightMode')) {
+  if (UserSettings.get('isInNightMode')) {
     Template.settingsdropdown.switchToNightMode();
   }
 }
 
 Template.settingsdropdown.switchToNightMode = function (){
-    //BODY
-    $('.pushable').addClass('nightmode');
-    $('.pusher').addClass('nightmode');
-    $('.article').addClass('nightmode');
-    $('body').addClass('nightmode');
+  //BODY
+  $('.pushable').addClass('nightmode');
+  $('.pusher').addClass('nightmode');
+  $('.article').addClass('nightmode');
+  $('body').addClass('nightmode');
 
-    //TEXT
-    $('.customlink').addClass('nightmodetext');
-    $('.customtitlelink').addClass('nightmodetext');
-    $('.dtubesidebartitle').addClass('nightmodetext');
-    $('.dtubesidebaricon').addClass('nightmodetext');
-    $('.videosnaprest').addClass('nightsecondarytext');
-    $('.videosnapauthor').addClass('nightsecondarytext');
-    $('.videosnaptime').addClass('nightsecondarytext');
-    $('.videosnapdescription').addClass('nightsecondarytext');
-    $('.videosnapdescriptionmobile').addClass('nightsecondarytext');
-    $('.verticalvideosnaptitle').addClass('nightmodetext');
-    $('.ui.item').addClass('nightmodetext');
-    $('.menuitem').addClass('nightmodetext');
-    $('#sidebar a').addClass('nightsecondarytext');
-    $('.ui.toggle.checkbox label').addClass('nightmodetext');
-    $('.ui.form .field > label').addClass('nightmodetext');
-    $('.text').addClass('nightmodetext');
-    $('.channelLink > a').addClass('nightmodetext');
-    $('.header').addClass('nightmodetext');
-    $('.ui.author').addClass('nightmodetext');
-    $('.ui.votebutton').addClass('nightmodetext');
-    $('.ui.comments .comment .metadata').addClass('nightsecondarytext');
-    $('.description').addClass('nightsecondarytext');
-    $('.videoshowmore').addClass('nightmodetext');
-    $('.buttonlabel').addClass('nightbutton');
-    $('.ui.checkbox label').addClass('nightmodetext');
+  //TEXT
+  $('.customlink').addClass('nightmodetext');
+  $('.customtitlelink').addClass('nightmodetext');
+  $('.dtubesidebartitle').addClass('nightmodetext');
+  $('.dtubesidebaricon').addClass('nightmodetext');
+  $('.videosnaprest').addClass('nightsecondarytext');
+  $('.videosnapauthor').addClass('nightsecondarytext');
+  $('.videosnaptime').addClass('nightsecondarytext');
+  $('.videosnapdescription').addClass('nightsecondarytext');
+  $('.videosnapdescriptionmobile').addClass('nightsecondarytext');
+  $('.verticalvideosnaptitle').addClass('nightmodetext');
+  $('.ui.item').addClass('nightmodetext');
+  $('.menuitem').addClass('nightmodetext');
+  $('#sidebar a').addClass('nightsecondarytext');
+  $('.ui.toggle.checkbox label').addClass('nightmodetext');
+  $('.ui.form .field > label').addClass('nightmodetext');
+  $('.text').addClass('nightmodetext');
+  $('.channelLink > a').addClass('nightmodetext');
+  $('.header').addClass('nightmodetext');
+  $('.ui.author').addClass('nightmodetext');
+  $('.ui.votebutton').addClass('nightmodetext');
+  $('.ui.comments .comment .metadata').addClass('nightsecondarytext');
+  $('.description').addClass('nightsecondarytext');
+  $('.videoshowmore').addClass('nightmodetext');
+  $('.buttonlabel').addClass('nightbutton');
+  $('.ui.checkbox label').addClass('nightmodetext');
 
-    $('.ui.segments > .segment').addClass('nightsegment');
-    $('.commentbutton').addClass('nightmodetext');
-    $('.ui.comments .comment .actions a').addClass('nightsecondarytext');
-    $('.owl-prev, .owl-next').addClass('nightmodegray');
+  $('.ui.segments > .segment').addClass('nightsegment');
+  $('.commentbutton').addClass('nightmodetext');
+  $('.ui.comments .comment .actions a').addClass('nightsecondarytext');
+  $('.owl-prev, .owl-next').addClass('nightmodegray');
 
-    //COMPONENTS
-    $('.menu').addClass('nightmode');
-    $('.ui.segment').addClass('nightmode');
-    $('.ui.card').addClass('nightmode');
-    $('.sidebarlink').addClass('nightmodegray');
-    $('.ui.secondary.segment').addClass('nightmodegray');
-    $('.ui.header').addClass('nightmodetext');
-    $('.item').addClass('nightmodetext');
+  //COMPONENTS
+  $('.menu').addClass('nightmode');
+  $('.ui.segment').addClass('nightmode');
+  $('.ui.card').addClass('nightmode');
+  $('.sidebarlink').addClass('nightmodegray');
+  $('.ui.secondary.segment').addClass('nightmodegray');
+  $('.ui.header').addClass('nightmodetext');
+  $('.item').addClass('nightmodetext');
 
-    $('.main.menu.fixed').addClass('nightmode');
-    $('.step').addClass('nightmode');
-    $('.sidebar').addClass('nightmodegray');
-    $('.sidebar.fixed').removeClass('inverted');
-    $('.ui.menu .item:before').addClass('nightmodeinverted');
-    $('.ellipsis.horizontal.icon').addClass('nightmode');
+  $('.main.menu.fixed').addClass('nightmode');
+  $('.step').addClass('nightmode');
+  $('.sidebar').addClass('nightmodegray');
+  $('.sidebar.fixed').removeClass('inverted');
+  $('.ui.menu .item:before').addClass('nightmodeinverted');
+  $('.ellipsis.horizontal.icon').addClass('nightmode');
 
-    //LOGO
-    $('.blacklogo').addClass('dsp-non');
-    $('.whitelogo').removeClass('dsp-non');
+  //LOGO
+  $('.blacklogo').addClass('dsp-non');
+  $('.whitelogo').removeClass('dsp-non');
 }
 
 Template.settingsdropdown.switchToNormalMode = function (){
-    //BODY
-$('.pushable').removeClass('nightmode');
-$('.pusher').removeClass('nightmode');
-$('.article').removeClass('nightmode');
-$('body').removeClass('nightmode');
+  //BODY
+  $('.pushable').removeClass('nightmode');
+  $('.pusher').removeClass('nightmode');
+  $('.article').removeClass('nightmode');
+  $('body').removeClass('nightmode');
 
-    //TEXT
-$('.customlink').removeClass('nightmodetext');
-$('.customtitlelink').removeClass('nightmodetext');
-$('.dtubesidebartitle').removeClass('nightmodetext');
-$('.dtubesidebaricon').removeClass('nightmodetext');
-$('.videosnaprest').removeClass('nightsecondarytext');
-$('.videosnapauthor').removeClass('nightsecondarytext');
-$('.videosnaptime').removeClass('nightsecondarytext');
-$('.videosnapdescription').removeClass('nightsecondarytext');
-$('.videosnapdescriptionmobile').removeClass('nightsecondarytext');
-$('.verticalvideosnaptitle').removeClass('nightmodetext');
-$('.ui.item').removeClass('nightmodetext');
-$('.menuitem').removeClass('nightmodetext');
-$('.dtubesidebarmenu a').removeClass('nightsecondarytext');
-$('.ui.toggle.checkbox label').removeClass('nightmodetext');
-$('.ui.form .field > label').removeClass('nightmodetext');
-$('.text').removeClass('nightmodetext');
-$('.channelLink > a').removeClass('nightmodetext');
-$('.header').removeClass('nightmodetext');
-$('.ui.author').removeClass('nightmodetext');
-$('.ui.votebutton').removeClass('nightmodetext');
-$('.ui.comments .comment .metadata').removeClass('nightsecondarytext');
-$('.description').removeClass('nightsecondarytext');
-$('.videoshowmore').removeClass('nightmodetext');
-$('.buttonlabel').removeClass('nightbutton');
-$('.ui.checkbox label').removeClass('nightmodetext');
+  //TEXT
+  $('.customlink').removeClass('nightmodetext');
+  $('.customtitlelink').removeClass('nightmodetext');
+  $('.dtubesidebartitle').removeClass('nightmodetext');
+  $('.dtubesidebaricon').removeClass('nightmodetext');
+  $('.videosnaprest').removeClass('nightsecondarytext');
+  $('.videosnapauthor').removeClass('nightsecondarytext');
+  $('.videosnaptime').removeClass('nightsecondarytext');
+  $('.videosnapdescription').removeClass('nightsecondarytext');
+  $('.videosnapdescriptionmobile').removeClass('nightsecondarytext');
+  $('.verticalvideosnaptitle').removeClass('nightmodetext');
+  $('.ui.item').removeClass('nightmodetext');
+  $('.menuitem').removeClass('nightmodetext');
+  $('.dtubesidebarmenu a').removeClass('nightsecondarytext');
+  $('.ui.toggle.checkbox label').removeClass('nightmodetext');
+  $('.ui.form .field > label').removeClass('nightmodetext');
+  $('.text').removeClass('nightmodetext');
+  $('.channelLink > a').removeClass('nightmodetext');
+  $('.header').removeClass('nightmodetext');
+  $('.ui.author').removeClass('nightmodetext');
+  $('.ui.votebutton').removeClass('nightmodetext');
+  $('.ui.comments .comment .metadata').removeClass('nightsecondarytext');
+  $('.description').removeClass('nightsecondarytext');
+  $('.videoshowmore').removeClass('nightmodetext');
+  $('.buttonlabel').removeClass('nightbutton');
+  $('.ui.checkbox label').removeClass('nightmodetext');
 
-$('.ui.segments > .segment').removeClass('nightsegment');
-$('.commentbutton').removeClass('nightmodetext');
-$('.ui.comments .comment .actions a').removeClass('nightsecondarytext');
-$('.owl-prev, .owl-next').removeClass('nightmodegray');
+  $('.ui.segments > .segment').removeClass('nightsegment');
+  $('.commentbutton').removeClass('nightmodetext');
+  $('.ui.comments .comment .actions a').removeClass('nightsecondarytext');
+  $('.owl-prev, .owl-next').removeClass('nightmodegray');
 
-    //COMPONENTS
-$('.menu').removeClass('nightmode');
-$('.ui.segment').removeClass('nightmode');
-$('.ui.card').removeClass('nightmode');
-$('.sidebarlink').removeClass('nightmodegray');
-$('.ui.secondary.segment').removeClass('nightmodegray');
-$('.ui.header').removeClass('nightmodetext');
-$('.item').removeClass('nightmodetext');
+  //COMPONENTS
+  $('.menu').removeClass('nightmode');
+  $('.ui.segment').removeClass('nightmode');
+  $('.ui.card').removeClass('nightmode');
+  $('.sidebarlink').removeClass('nightmodegray');
+  $('.ui.secondary.segment').removeClass('nightmodegray');
+  $('.ui.header').removeClass('nightmodetext');
+  $('.item').removeClass('nightmodetext');
 
-$('.main.menu.fixed').removeClass('nightmode');
-$('.step').removeClass('nightmode');
-$('.sidebar').removeClass('nightmodegray');
-$('.sidebar.fixed').addClass('inverted');
-$('.ui.menu .item:before').removeClass('nightmodeinverted');
-$('.ellipsis.horizontal.icon').removeClass('nightmode');
+  $('.main.menu.fixed').removeClass('nightmode');
+  $('.step').removeClass('nightmode');
+  $('.sidebar').removeClass('nightmodegray');
+  $('.sidebar.fixed').addClass('inverted');
+  $('.ui.menu .item:before').removeClass('nightmodeinverted');
+  $('.ellipsis.horizontal.icon').removeClass('nightmode');
 
-//LOGO
-$('.blacklogo').removeClass('dsp-non');
-$('.whitelogo').addClass('dsp-non');
+  //LOGO
+  $('.blacklogo').removeClass('dsp-non');
+  $('.whitelogo').addClass('dsp-non');
 }
