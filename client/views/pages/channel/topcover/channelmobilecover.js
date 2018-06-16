9edf41677387ebc5443fb44d573b9d03a62bbcd5
happyrobotstudio <happyrobotstudio@gmail.com>
2018-06-16 23:29:55 +1000

    Loading behaviour of /#!/c/username shows the 'randomBackgroundColor' breifly before the author.json_metadata.profile.cover_image is shown (when one exists), an {{#if author}} clause has been added in channeldesktopcover.html and channelmobilecover.html in order to wait until the user object is fully loaded, before revealing the header

--- client/views/pages/channel/topcover/channelmobilecover.js
+++ client/views/pages/channel/topcover/channelmobilecover.js
@@ -1,21 +1,42 @@
 Template.channelmobilecover.helpers({
     mainUser: function () {
-        return Users.findOne({ username: Session.get('activeUsername') })
-      },
-      user: function () {
-        return {
-          name: FlowRouter.getParam("author")
-        }
-      },
-      author: function () {
-        return ChainUsers.findOne({ name: FlowRouter.getParam("author") })
-      },
-      activeUser: function () {
-        return Session.get('activeUsername')
-      },
-      subCount: function () {
-        var subCount = SubCounts.findOne({ account: FlowRouter.getParam("author") })
-        if (!subCount || !subCount.follower_count) return 0
-        return subCount.follower_count;
+      return Users.findOne({ username: Session.get('activeUsername') })
+    },
+    user: function () {
+      return {
+        name: FlowRouter.getParam("author")
       }
+    },
+    author: function () {
+      return ChainUsers.findOne({ name: FlowRouter.getParam("author") })
+    },
+    activeUser: function () {
+      return Session.get('activeUsername')
+    },
+    subCount: function () {
+      var subCount = SubCounts.findOne({ account: FlowRouter.getParam("author") })
+      if (!subCount || !subCount.follower_count) return 0
+      return subCount.follower_count;
+    },
+    randomBackgroundColor: function () {
+      var rnd = Math.floor(Math.random() * (5 - 1 + 1)) + 1;
+      var bgcolor = ''
+      switch (rnd) {
+        case 1:
+          bgcolor = 'channelbga';
+          break;
+        case 2:
+          bgcolor = 'channelbgb';
+          break;
+        case 3:
+          bgcolor = 'channelbgc';
+          break;
+        case 4:
+          bgcolor = 'channelbgd';
+          break;
+        default:
+          bgcolor = 'channelbge';
+      }
+      return bgcolor
+    }
   })
