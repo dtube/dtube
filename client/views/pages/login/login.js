74670dadbe2b9b39d82637fbd855f954c8a532a2
dtube network, vaultec <47548474+dtubenetwork@users.noreply.github.com>
2019-02-21 09:23:52 -0800

    keychain integration
    Commits:
    https://gitlab.com/vaultec/dtube/commit/582b58e2b59f694f1666b7d018c761c9e3b7c30c
    https://gitlab.com/vaultec/dtube/commit/dab45a4308d1216cb9d970caf188de6af09e47a6
    https://gitlab.com/vaultec/dtube/commit/e3ed3e9d8a1aee1a49ffdfaa099883406bd174e5
    https://gitlab.com/vaultec/dtube/commit/bbc54d216fd4f1faac78af3ebfefcce6a04c4690

--- client/views/pages/login/login.js
+++ client/views/pages/login/login.js
@@ -17,6 +17,48 @@ Template.login.success = function(activeUsername, noreroute) {
 }
 
 Template.login.events({
+  'click #loginkeychain': function(event) {
+    event.preventDefault()
+    if(window.steem_keychain) {
+        // Steem Keychain extension installed...
+    } else {
+        // Steem Keychain extension not installed...
+        toastr.error(translate('LOGIN_ERROR_KEYCHAIN_NOT_INSTALLED'), translate('ERROR_TITLE'))
+        return;
+    }
+    steem_keychain.requestHandshake(function() {
+        console.log('Handshake received!'); 
+    }); 
+    console.log(document.getElementById("username").value);
+    var username = document.getElementById("username").value.toLowerCase().replace('@','');
+    steem_keychain.requestSignBuffer(username, "dtube_login-" + String(Math.random()), "Posting", function(response) {
+        console.log(response);
+        if(response.success === true) {
+            var currentUser = Session.get('activeUsername')
+            var username = response.data.username;
+            if (currentUser == username)
+            {
+              toastr.error(translate('LOGIN_ERROR_ALREADY_LOGGED'), translate('ERROR_TITLE'))
+              return
+            }
+            var user = {
+               username: response.data.username,
+               type: "keychain"
+            }
+            Waka.db.Users.upsert(user, function() {
+              Users.remove({})
+              Users.refreshLocalUsers(function(err) {
+                Template.login.success(user.username)
+              })
+          })
+        } else {
+          toastr.error(translate('LOGIN_ERROR_AUTHENTIFICATION_FAILED'), translate('ERROR_TITLE'));
+        }
+    });
+    /*steem_keychain.requestVerifyKey(Session.get('activeUsername'), "dtube_login-" + String(Math.random()), "Posting", function(response) {
+        console.log(response);
+    });*/
+  },
   'click #loginbuttonsc2': function(event) {
     event.preventDefault()
     var url = sc2.getLoginURL()
@@ -45,7 +87,8 @@ Template.login.events({
       }
       var chainuser = result[0]
       var user = {
-        privatekey: event.target.privatekey.value
+        privatekey: event.target.privatekey.value,
+        type: "posting"
       }
       try {
         user.publickey = steem.auth.wifToPublic(user.privatekey)
