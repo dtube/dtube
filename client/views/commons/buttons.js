8028e57d1d2d1906311d132ca27ea97faf28d7f5
Adrien M <adrien@d.tube>
2019-06-06 22:19:29 +0200

    improve search results

--- client/views/commons/buttons.js
+++ client/views/commons/buttons.js
@@ -14,18 +14,28 @@
       $('.transferdtc').hide()
     },
     'click #confirmTransfer': function () {
-      var amount = Math.floor(parseFloat($('#transfer_amount').val())*100)
-      var memo = $('#transfer_memo').val()
-      var receiver = FlowRouter.getParam("author")
-      broadcast.avalon.transfer(receiver, amount, memo, function(err, res) {
-        if (err) toastr.error(Meteor.blockchainError(err))
-        else {
-          toastr.success(translate('TRANSFER_SUCCESS_DESC', $('#transfer_amount').val(), receiver), translate('TRANSFER_SUCCESS_TITLE'))
-          $('.transferdtc').hide()
-        }
-      })
+      Template.buttontransfer.transfer()
     },
   })
+
+  Template.buttontransfer.transfer = function() {
+    $("#confirmTransfer").addClass('disabled')
+    $("#confirmTransfer > i.check").addClass('dsp-non')
+    $("#confirmTransfer > i.loading").removeClass('dsp-non')
+    var amount = Math.floor(parseFloat($('#transfer_amount').val())*100)
+    var memo = $('#transfer_memo').val()
+    var receiver = FlowRouter.getParam("author")
+    broadcast.avalon.transfer(receiver, amount, memo, function(err, res) {
+      $("#confirmTransfer").removeClass('disabled')
+      $("#confirmTransfer > i.loading").addClass('dsp-non')
+      $("#confirmTransfer > i.check").removeClass('dsp-non')
+      if (err) toastr.error(Meteor.blockchainError(err))
+      else {
+        toastr.success(translate('TRANSFER_SUCCESS_DESC', $('#transfer_amount').val(), receiver), translate('TRANSFER_SUCCESS_TITLE'))
+        $('.transferdtc').hide()
+      }
+    })
+  }
 
   Template.buttontransfersmall.events({
     'click .transferdtcbtn': function () {
@@ -35,21 +45,6 @@
       $('.transferdtc').hide()
     },
     'click #confirmTransfer': function () {
-      $("#confirmTransfer").addClass('disabled')
-      $("#confirmTransfer > i.check").addClass('dsp-non')
-      $("#confirmTransfer > i.loading").removeClass('dsp-non')
-      var amount = Math.floor(parseFloat($('#transfer_amount').val())*100)
-      var memo = $('#transfer_memo').val()
-      var receiver = FlowRouter.getParam("author")
-      broadcast.avalon.transfer(receiver, amount, memo, function(err, res) {
-        $("#confirmTransfer").removeClass('disabled')
-        $("#confirmTransfer > i.loading").addClass('dsp-non')
-        $("#confirmTransfer > i.check").removeClass('dsp-non')
-        if (err) toastr.error(Meteor.blockchainError(err))
-        else {
-          toastr.success(translate('TRANSFER_SUCCESS_DESC', $('#transfer_amount').val(), receiver), translate('TRANSFER_SUCCESS_TITLE'))
-          $('.transferdtc').hide()
-        }
-      })
+      Template.buttontransfer.transfer()
     },
   })
