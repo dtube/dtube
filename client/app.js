5a732c1f30226a4f0dd26c4d9debf7bc7e85383f
Adrien M <adrien@d.tube>
2019-07-31 15:45:42 +0200

    update toastr settings

--- client/app.js
+++ client/app.js
@@ -62,17 +62,17 @@ Meteor.startup(function(){
     "debug": false,
     "newestOnTop": true,
     "progressBar": true,
-    "positionClass": "toast-bottom-right",
+    "positionClass": "toast-top-right",
     "preventDuplicates": false,
     "onclick": null,
     "showDuration": "300",
     "hideDuration": "1000",
     "timeOut": "5000",
     "extendedTimeOut": "1000",
-    "showEasing": "swing",
+    "showEasing": "linear",
     "hideEasing": "linear",
-    "showMethod": "fadeIn",
-    "hideMethod": "fadeOut"
+    "showMethod": "linear",
+    "hideMethod": "linear"
   }
 
   if (Session.get('remoteSettings').warning)
