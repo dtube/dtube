6638e6c72de2b1c0d552c032af27036fb5310855
Adrien M <adrien@d.tube>
2019-09-29 19:05:11 +0200

    remove logging

--- client/collections/activities.js
+++ client/collections/activities.js
@@ -4,7 +4,6 @@ Activities.getAccountHistory = function (username, cb) {
     if (Activities.find({ username: username }).fetch().length) {
         var lastActivity = Activities.findOne({ username: FlowRouter.getParam("author") }, { sort: { n: 1 } }).n
         avalon.getAccountHistory(username, lastActivity, function (e, r) {
-            console.log(e,r)
             for (let i = 0; i < r.length; i++) {
                 if (!r[i].txs || r[i].txs.length < 1) return
                 for (let y = 0; y < r[i].txs.length; y++)
