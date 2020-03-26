255101236e5df56ee82c82f84ade28c9adae3878
Adrien M <adrien@d.tube>
2020-03-26 22:44:31 +0100

    display blockchain logins in a totally random order

--- client/views/pages/login/login.js
+++ client/views/pages/login/login.js
@@ -31,6 +31,14 @@ Template.login.helpers({
   },
   isNightMode: () => {
     return UserSettings.get('isInNightMode')
+  },
+  randomOrder: () => {
+    var a = ['steem', 'hive', 'dtc']
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
   }
 })
 
