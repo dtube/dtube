57a6b7250f90c72861e21c1467d04e19c7e8a1d8
Adrien M <adrien@d.tube>
2020-04-27 21:57:55 +0200

    remember which announcement have been closed

--- client/views/commons/announcement.js
+++ client/views/commons/announcement.js
@@ -1,5 +1,12 @@
+// always increment this id when changing message in production
+var messageId = 1
+
 Template.announcement.rendered = function() {
+    if (!localStorage.getItem('lastAnnouncementRead') || localStorage.getItem('lastAnnouncementRead') < messageId)
+        $('.announcement').parent().show()   
+
     $('.announcement .close').on('click', function() {
-      $(this).closest('.message').transition('fade')
+        $(this).closest('.message').transition('fade')
+        localStorage.setItem('lastAnnouncementRead', messageId)
     })
 }
