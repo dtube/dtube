5fdd9b32b8b7fb0a45a9bac60489e3539a2d876e
fasolo97 <79fasolo97@gmail.com>
2024-01-18 22:05:07 +0100

    Update ads and javalon code

--- client/avalon.js
+++ client/avalon.js
@@ -1,13 +1,13 @@
-const javalon = require('javalon')
+import avalon from 'javalon2';
 
 if (localStorage.getItem('avalonAPI')) {
-    javalon.init({api: localStorage.getItem('avalonAPI')})
+    avalon.init({api: localStorage.getItem('avalonAPI')})
 } else {
-    javalon.init({api: 'https://avalon.d.tube'})
+    avalon.init({api: 'https://avalon.d.tube'})
 }
 
-Session.set('avalonAPI',javalon.config.api)
+Session.set('avalonAPI',avalon.config.api)
 
 // javalon.init({api: 'https://bran.nannal.com:443'})
 //javalon.init({api: 'http://localhost:3001'})
-window.avalon = javalon
+window.avalon = avalon
